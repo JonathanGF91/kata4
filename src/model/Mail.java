52730f2ef965ca905f3effa550a2c2bf8af39028
Jonathan Gómez <joni.gf91@gmail.com>
2021-01-16 20:07:34 +0000

    methods of the Mail class

--- src/model/Mail.java
+++ src/model/Mail.java
@@ -3,4 +3,13 @@ package model;
 
 public class Mail {
     
+    private final String mail;
+
+    public Mail(String mail) {
+        this.mail = mail;
+    }
+    
+    public String getDomain(){
+        return mail.split("@")[1];
+    }
 }
